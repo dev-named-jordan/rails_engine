a7387a94cd2ef0dae26dccbde73ed914a700292d
Jordan Beck <jordanfbeck@gmail.com>
2021-04-04 13:59:40 -0600

    Add code for create and update

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
 	factory :item do
-		name { Faker::Games::LeagueOfLegends.champion }
+		name { Faker::Games::LeagueOfLegends.summoner_spell }
 		description { Faker::Lorem.word}
 		unit_price { Faker::Number.decimal(l_digits: 3, r_digits:2)}
 		merchant
