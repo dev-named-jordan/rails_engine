00ef3383dbaf3654569847e88dc2a10d121b5967
Jordan Beck <jordanfbeck@gmail.com>
2021-04-04 13:58:35 -0600

    Add feature test for item update

---
+++ spec/requests/api/v1/items/item_update_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "can update an existing item" do
+    merchant = create(:merchant)
+    id = create(:item, merchant_id: merchant.id).id
+    previous_name = Item.last.name
+    item_params = { name: "value2" }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    # We include this header to make sure that these params are passed as JSON rather than as plain text
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("value2")
+  end
+end
